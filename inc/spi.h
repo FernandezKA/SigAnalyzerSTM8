51a160d2b642cb5e153af55f7e1d74632c424fe9
Fernandez K.A <fernandes.kir@yandex.ru>
2021-08-09 14:24:21 +0300

    Edit SPI pins

--- inc/spi.h
+++ inc/spi.h
@@ -2,9 +2,9 @@
 #define _spi_h_
 #include "general.h"
 #define SPI_SCK GPIO_PIN_3  //SCLK
-#define SPI_SDI GPIO_PIN_2  //MOSI
-#define SPI_NSS GPIO_PIN_1  //NSS
-#define SPI_PORT GPIOA //SPI port
+#define SPI_SDI GPIO_PIN_4  //MOSI
+#define SPI_NSS GPIO_PIN_5  //NSS
+#define SPI_PORT GPIOC //SPI port
 void _spi_start(void);
 void _spi_stop(void);
 void _spi_send(unsigned char d);
