4d53fd4a230a06cad2ccf56b21043a1070c3aff0
Fernandez K.A <fernandes.kir@yandex.ru>
2021-08-19 17:06:26 +0300

    Check HSI with prescaler 2 for testing

--- src/clocking.c
+++ src/clocking.c
@@ -1,10 +1,11 @@
 #include "clocking.h"
 void vClock_Config(void){
  //CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV2);
- CLK->ECKR|=CLK_ECKR_HSEEN;
- CLK->SWCR|=CLK_SWCR_SWEN;
- while(((CLK->ECKR & CLK_ECKR_HSERDY) != CLK_ECKR_HSERDY)) {asm("nop");}
- CLK->CKDIVR = 0x00;
- CLK->SWR = 0xB4;        //HSE AS MASTER CLOCK SOURCE
- while((CLK->SWCR & CLK_SWCR_SWIF) != CLK_SWCR_SWIF) {asm("nop");}
+ //CLK->ECKR|=CLK_ECKR_HSEEN;
+ //CLK->SWCR|=CLK_SWCR_SWEN;
+ //while(((CLK->ECKR & CLK_ECKR_HSERDY) != CLK_ECKR_HSERDY)) {asm("nop");}
+ //CLK->CKDIVR = 0x00;
+  CLK->CKDIVR = 0x01;
+ //CLK->SWR = 0xB4;        //HSE AS MASTER CLOCK SOURCE
+ //while((CLK->SWCR & CLK_SWCR_SWIF) != CLK_SWCR_SWIF) {asm("nop");}
 }
